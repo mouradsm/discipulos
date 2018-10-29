d6477ba784264827e6b3cd3e809cdbbb5fb05c00
Diego Moura <diegosouza20049@hotmail.com>
2018-10-29 18:12:53 -0300

    regras do lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,41 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module',
-    "ecmaVersion": 6
   },
   env: {
     browser: true,
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue'
   ],
   // add your custom rules here
   'rules': {
+     // allow async-await
+    'generator-star-spacing': 'off',
+
     // allow paren-less arrow functions
     'arrow-parens': 0,
-    'semi': 2,
-    // allow async-await
-    'generator-star-spacing': 0,
+    'one-var': 0,
+
+    'import/first': 0,
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+    'vue/max-attributes-per-line': 0,
+
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }

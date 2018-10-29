e4ed3e5840dc81cb0161b4a2685c3eacf076b09d
Diego Moura <diegosouza20049@hotmail.com>
2018-10-29 19:19:30 -0300

    remoção das configuraçoes do firebase do main

--- src/main.js
+++ src/main.js
@@ -3,35 +3,28 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import {store} from './store'
-import firebase from 'firebase'
+import {
+  store
+} from './store'
+const fb = require('./firebaseConfig.js')
 
 Vue.use(Vuetify)
-firebase.initializeApp({
-  apiKey: 'AIzaSyC4sScaNw4l4G_l7uK54dw0eQcJjSk70UA',
-  authDomain: 'christian-manager.firebaseapp.com',
-  databaseURL: 'https://christian-manager.firebaseio.com',
-  projectId: 'christian-manager',
-  storageBucket: 'christian-manager.appspot.com',
-  messagingSenderId: '286250341384'
-
-})
 
 Vue.config.productionTip = false
+Vue.config.devtools = true
 
 /* eslint-disable no-new */
-const unsubscribe = firebase.auth()
-  .onAuthStateChanged((firebaseUser) => {
-    new Vue({
-      el: '#app',
-      router,
-      store,
-      render: h => h(App),
-      created () {
-        if (firebaseUser) {
-          store.dispatch('autoSignIn', firebaseUser)
-        }
+const unsubscribe = fb.auth.onAuthStateChanged((firebaseUser) => {
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App),
+    created () {
+      if (firebaseUser) {
+        store.dispatch('autoSignIn', firebaseUser)
       }
-    })
-    unsubscribe()
+    }
   })
+  unsubscribe()
+})

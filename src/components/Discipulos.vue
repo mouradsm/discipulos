00ea7a1416733edb7dfb9350f14f751ad80156c6
Diego Moura <diegosouza20049@hotmail.com>
2019-12-09 23:21:24 -0300

    Página para exibição dos membros

--- src/components/Discipulos.vue
+++ src/components/Discipulos.vue
@@ -1,16 +1,93 @@
 <template>
-  <v-container fluid>
-    <v-layout row wrap>
-      <v-flex xs12 class="text-xs-center" mt-5>
-        <h1>Discipulos page</h1>
-      </v-flex>
-      <v-flex xs12 class="text-xs-center" mt-3>
-        <p>This is a user's home page</p>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-list two-line sub-header>
+          <v-subheader>Discipulos</v-subheader>
+          <template v-for="(item, index) in discipulos">
+              <v-list-tile :key="index.nome" @click="router.push({path: '/detalhe/' + item.uid})">
+                <v-list-tile-avatar>
+                  <img src="https://cdn.vuetifyjs.com/images/lists/1.jpg">
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{item.nome}}</v-list-tile-title>
+                  <v-list-tile-sub-title>{{item.cobertura}}</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider :key="index"></v-divider>
+          </template>
+          <!-- <template v-for="(item, index) in items">
+            <v-subheader
+              v-if="item.header"
+              :key="item.header"
+            >
+              {{ item.header }}
+            </v-subheader>
+
+            <v-divider
+              v-else-if="item.divider"
+              :key="index"
+              :inset="item.inset"
+            ></v-divider>
+
+            <v-list-tile
+              v-else
+              :key="item.title"
+              avatar
+            >
+              <v-list-tile-avatar>
+                <img :src="item.avatar">
+              </v-list-tile-avatar>
+
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template> -->
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-export default {}
+const fb = require('../firebaseConfig.js')
+import router from '../router'
+export default {
+  mounted () {
+    fb.db.collection('discipulos').get().then(spanshot => {
+      spanshot.forEach(doc => {
+        let discipulo = doc.data()
+        this.discipulos.push({'uid': doc.id, ...discipulo})
+      })
+    })
+  },
+  data () {
+    return {
+      router: router,
+      discipulos: [],
+      items: [
+        { header: 'Discipulos' },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          title: 'Brunch this weekend?',
+          subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          title: 'Oui oui',
+          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+        }
+      ]
+    }
+  }
+}
 </script>

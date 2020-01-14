5edde7f69965ddbd2d8187c4dc7542ea2b14b999
Diego Moura <diegosouza20049@hotmail.com>
2020-01-14 14:45:22 -0300

    melhoria no componente para deixar um pouco mais reutilizável

--- src/components/ListaDiscipulos.vue
+++ src/components/ListaDiscipulos.vue
@@ -13,6 +13,21 @@
                   <v-list-tile-title>{{item.nome}}</v-list-tile-title>
                   <v-list-tile-sub-title>{{item.cobertura}}</v-list-tile-sub-title>
                 </v-list-tile-content>
+                <v-list-tile-action v-show="detalhe">
+                  <v-btn icon @click="router.push({path: '/detalhe/' + item.uid})">
+                      <v-icon>arrow_forward</v-icon>
+                  </v-btn>
+                </v-list-tile-action>
+                <v-list-tile-action v-show="vinculo">
+                  <v-flex>
+                    <v-btn icon v-if="!isVinculo(item.uid)" @click="addVinculo(item.uid)" key="add">
+                      <v-icon color="green lighten-1">add</v-icon>
+                    </v-btn>
+                    <v-btn icon v-else @click="removeVinculo(item.uid)" key="remove">
+                      <v-icon color="red">clear</v-icon>
+                    </v-btn>
+                  </v-flex>
+                </v-list-tile-action>
               </v-list-tile>
               <v-divider v-if="!(index == filteredItens.length - 1)" :key="index"></v-divider>
           </template>
@@ -23,20 +38,14 @@
 </template>
 
 <script>
-import router from '../router'
-
 const fb = require('../firebaseConfig.js')
 
 export default {
   name: 'ListaDiscipulos',
-  computed: {
-    filteredItens () {
-      const search = this.filter.toLowerCase().trim()
-
-      if (!search) return this.discipulos
-
-      return this.discipulos.filter(d => d.nome.toLowerCase().indexOf(search) > -1)
-    }
+  props: {
+    filter: String,
+    vinculo: Boolean,
+    detalhe: Boolean
   },
   mounted () {
     fb.db.collection('discipulos').get().then(spanshot => {
@@ -46,12 +55,34 @@ export default {
       })
     })
   },
-  data () {
-    return {
-      router: router,
-      discipulos: []
+  computed: {
+    filteredItens () {
+      const search = this.filter.toLowerCase().trim()
+
+      if (!search) return this.discipulos
+
+      return this.discipulos.filter(d => d.nome.toLowerCase().indexOf(search) > -1)
     }
   },
-  props: ['filter']
+  data () {
+    return {
+      discipulos: [],
+      listaVinculos: []
+    }
+  },
+  methods: {
+    addVinculo (uid) {
+      this.listaVinculos.push(uid)
+    },
+    removeVinculo (uid) {
+      this.listaVinculos.splice(this.listaVinculos.indexOf(uid), 1)
+    },
+    isVinculo (uid) {
+      return this.listaVinculos.indexOf(uid) > -1
+    }
+  }
 }
 </script>
+<style scoped>
+
+</style>

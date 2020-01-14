fd0e06a0eae2a79710c8215949de48c1a5bfc91b
Diego Moura <diegosouza20049@hotmail.com>
2020-01-14 14:44:57 -0300

    refatoração para utilizar o componente de lista

--- src/components/Discipulos.vue
+++ src/components/Discipulos.vue
@@ -1,44 +1,32 @@
 <template>
   <v-layout row>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-list two-line sub-header>
-          <v-subheader >Discipulos</v-subheader>
-          <v-divider></v-divider>
-          <template v-for="(item, index) in discipulos">
-              <v-list-tile :key="item.nome" @click="router.push({path: '/detalhe/' + item.uid})">
-                <v-list-tile-avatar>
-                  <img src="https://cdn.vuetifyjs.com/images/lists/1.jpg">
-                </v-list-tile-avatar>
-                <v-list-tile-content>
-                  <v-list-tile-title>{{item.nome}}</v-list-tile-title>
-                  <v-list-tile-sub-title>{{item.cobertura}}</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <v-divider v-if="!(index == discipulos.length - 1)" :key="index"></v-divider>
-          </template>
-        </v-list>
-      </v-card>
+    <v-flex>
+     <v-text-field
+        class="mt-3"
+        append-icon="search"
+        v-model="query"
+        solo></v-text-field>
+
+        <ListaDiscipulos :filter="query" detalhe />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-const fb = require('../firebaseConfig.js')
 import router from '../router'
+import ListaDiscipulos from './ListaDiscipulos'
+
+// const fb = require('../firebaseConfig.js')
+
 export default {
-  mounted () {
-    fb.db.collection('discipulos').get().then(spanshot => {
-      spanshot.forEach(doc => {
-        let discipulo = doc.data()
-        this.discipulos.push({'uid': doc.id, ...discipulo})
-      })
-    })
+  components: {
+    ListaDiscipulos
   },
   data () {
     return {
       router: router,
-      discipulos: []
+      query: ''
+      // discipulos: []
     }
   }
 }
